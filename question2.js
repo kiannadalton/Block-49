889be1af9dcc8e3936d7c6a6fe4f75c27544d9b0
kiannadalton <kiannadalton@aol.com>
2024-09-25 18:37:49 -0400

    question2 done

--- question2.js
+++ question2.js
@@ -1,21 +1,20 @@
 // Question 2
-// Create a Node class in JavaScript to build parts of a binary tree. Then, make an 'isSameTree' function. This function checks two binary trees and tells if they are the same or not. Test your function by making two binary trees and comparing them. For an input binary tree with nodes [1,2,3], 'isSameTree' should return 'true' for the same output [1,2,3] and 'false' otherwise. 
-
+// Create a Node class in JavaScript to build parts of a binary tree. Then, make an 'isSameTree' function. This function checks two binary trees and tells if they are the same or not. Test your function by making two binary trees and comparing them. For an input binary tree with nodes [1,2,3], 'isSameTree' should return 'true' for the same output [1,2,3] and 'false' otherwise.
 
 class Node {
-  // Binary tree data structure
-  constructor(val, left = null, right = null) {
-    this.val = val;
+  // This is the binary tree data structure
+  constructor(value, left = null, right = null) {
+    this.value = value;
     this.left = left;
     this.right = right;
   }
 }
 
-function isSameTree(p, q) {
-  // Creating function to check if the trees are same
+function checkSameTree(a, b) {
+  // this is a function to check if the trees are same
   let queue = [];
-  queue.push(p);
-  queue.push(q);
+  queue.push(a);
+  queue.push(b);
 
   while (queue.length > 0) {
     let node1 = queue.shift();
@@ -25,7 +24,7 @@ function isSameTree(p, q) {
       continue;
     }
 
-    if (!node1 || !node2 || node1.val !== node2.val) {
+    if (!node1 || !node2 || node1.value !== node2.value) {
       return false;
     }
 
@@ -39,11 +38,12 @@ function isSameTree(p, q) {
   return true;
 }
 
-// Testing the code:
-let p = new Node(1, new Node(2), new Node(3));
-let q = new Node(1, new Node(2), new Node(3));
-console.log(isSameTree(p, q)); // This should print 'true'
+let a = new Node(1, new Node(2), new Node(3));
+let b = new Node(1, new Node(2), new Node(3));
+console.log(checkSameTree(a, b)); // This should print 'true'
 
-p = new Node(1, new Node(2));
-q = new Node(1, null, new Node(2));
-console.log(isSameTree(p, q)); // This should print 'false'
+a = new Node(1, null, new Node(2));
+b = new Node(1, new Node(2));
+console.log(checkSameTree(a, b)); // This should print 'false'
+
+// outputs work
